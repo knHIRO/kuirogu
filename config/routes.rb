1e21da48467d59fd60fc5a8607e474c7ae4856d4
EC2 Default User <ec2-user@ip-172-31-12-53.ap-northeast-1.compute.internal>
2023-01-26 12:04:27 +0000

    Sign screen Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  namespace :admin do
+
+  end
+  namespace :public do
+
+  end
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+root to: "public/homes#top"
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+get 'admin' => 'admin/homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
