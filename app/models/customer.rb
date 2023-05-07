6755b4e9a5084984f13571931bb887ae123c9466
EC2 Default User <ec2-user@ip-172-31-12-53.ap-northeast-1.compute.internal>
2023-05-07 23:41:13 +0000

    Image error Commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -28,11 +28,15 @@ class Customer < ApplicationRecord
   #   profile_image.variant(resize_to_limit: [width, height]).processed
   # end
   def get_profile_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+  unless profile_image.attached?
+    file_path = Rails.root.join('app', 'assets', 'images', 'no_image.jpg')
+    unless File.exist?(file_path)
+      # no_image.jpgファイルが存在しない場合、作成する
+      FileUtils.cp(Rails.root.join('app', 'assets', 'images', 'default_image.jpg'), file_path)
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+    profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def active_for_authentication?
