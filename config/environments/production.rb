e6a5b6270665d220b11259fd437aac537235c6b3
EC2 Default User <ec2-user@ip-172-31-12-53.ap-northeast-1.compute.internal>
2023-05-08 15:59:43 +0000

    Image error Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_console.whitelisted_ips = '43.206.221.69'
-
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
