19ff95d6e94bcc07229eca925f8448633074c6f1
EC2 Default User <ec2-user@ip-172-31-12-53.ap-northeast-1.compute.internal>
2023-05-19 22:46:07 +0000

    Image error Commit

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -20,6 +20,31 @@ class Public::SessionsController < Devise::SessionsController
 
   # protected
 
+  def guest_sign_in
+    customer = Customer.guest
+    sign_in customer
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def guest_sign_in
+    customer = Customer.find_or_create_by!(email: 'guest@example.com') do |customer|
+      customer.first_name = 'Guest'
+      customer.last_name = 'User'
+      customer.last_name_kana = 'ゲスト'
+      customer.first_name_kana = 'ユーザー'
+      customer.telephone_number = '09012345678'
+      customer.password = SecureRandom.urlsafe_base64
+      # user.skip_confirmation!  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+    sign_in customer
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+
+
+
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
