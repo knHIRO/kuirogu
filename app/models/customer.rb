666bd146092b055b6eacc46bbb0504f20034545b
EC2 Default User <ec2-user@ip-172-31-12-53.ap-northeast-1.compute.internal>
2023-06-03 06:58:40 +0000

    Image error Commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -50,6 +50,7 @@ class Customer < ApplicationRecord
   def get_profile_image(width,height)
      unless profile_image.attached?
        file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+     #binding.pry
        profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
      end
     profile_image.variant(resize_to_limit: [width, height]).processed
